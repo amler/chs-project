db80ac4f798465d47b74f8b71b50d514193380a4
Heather Amler <heatheramler@yahoo.com>
2015-05-12 23:44:45 -0400

    Added form modal

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,16 @@
-console.log('main.js');
+DemandConnectSettings = {
+        orgId: '00Di0000000HPkiEAG',
+        specialCampaigns: {
+
+        },
+        campaigns: {
+                hcmacid: 'HC4__ExternalId__c'
+        }
+};
+
+DemandConnectFormSettings = {
+	debug: false, /* If true, does NOT send data to the server */
+	logErrors: true, /* If true, errors will be logged on the server */
+	defaultParameterName: 'hcmacid', /* Default URL parameter name for this Web Form */
+	defaultParameterValue: 'a0bi000000N1v7bAAB' /* Default (catch-all) Campaign Tactic for this Web Form */
+};
